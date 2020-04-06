65f1b2fd58f887c5a9ef97c23d4f601879069d83
razchn@gmail.com <razchn@gmail.com>
2020-04-06 17:55:06 +0300

    add HOST

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from collector import collect
 from metrics import CUSTOM_METRICS_LIST
 
 PORT = environ.get('PORT')
+HOST = environ.get('HOST')
 
 
 def get_metrics_list(custom_metircs_list: list = CUSTOM_METRICS_LIST) -> list:
@@ -14,7 +15,7 @@ def get_metrics_list(custom_metircs_list: list = CUSTOM_METRICS_LIST) -> list:
 
 if __name__ == '__main__':
     metrics_list = get_metrics_list()
-    runner.run_async(metrics_list, '127.0.0.1', PORT, collect)
+    runner.run_async(metrics_list, HOST, PORT, collect)
 
 
 
