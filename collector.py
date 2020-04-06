5d9f22b62c3721d0b4045c6b7174d165a7d1aa04
razchn@gmail.com <razchn@gmail.com>
2020-04-06 16:39:12 +0300

    add COLLECT_INTERVAL

--- collector.py
+++ collector.py
@@ -1,10 +1,11 @@
+import asyncio
 from os import environ
 
-from interfaces.openshift_api import Api
 from handlers.nodes import get_nodes_names_list
-from tasks.resource_init import delete_resources, create_resources
+from interfaces.openshift_api import Api
 from tasks.builds import build_task
-from utils.metrics import update_build_duration_metric,update_success_rate_metric
+from tasks.resource_init import delete_resources, create_resources
+from utils.metrics import update_build_duration_metric, update_success_rate_metric
 
 ENV_URL = environ.get('ENV_URL')
 DEFAULT_TOKEN = environ.get('DEFAULT_TOKEN')
@@ -12,7 +13,7 @@ ENV_NAME = environ.get('ENV_NAME')
 NAMESPACE = environ.get('OPENSHIFT_BUILD_NAMESPACE')
 TIMEOUT = environ.get('TIMEOUT')
 WATCH_SLEEP = environ.get('WATCH_SLEEP')
-
+COLLECT_INTERVAL = environ.get('COLLECT_INTERVAL')
 
 async def collect(metrics_list: list) -> None:
     url = ENV_URL
@@ -21,6 +22,7 @@ async def collect(metrics_list: list) -> None:
     namespace = NAMESPACE
     timeout = int(TIMEOUT)
     watch_sleep = int(WATCH_SLEEP)
+    collect_inetrval = int(COLLECT_INTERVAL)
 
     success_rate_metric = metrics_list[0]
     build_duration_metric = metrics_list[1]
@@ -40,3 +42,5 @@ async def collect(metrics_list: list) -> None:
 
         update_build_duration_metric(build_duration_metric, results_json_list)
         update_success_rate_metric(success_rate_metric, results_json_list)
+
+        await asyncio.sleep(collect_inetrval)
--- tasks/resource_init.py
+++ tasks/resource_init.py
@@ -11,8 +11,8 @@ async def create_resources(openshift_api: Api, namespace: str, nodes_names_list:
     buildconfigs_names_list = get_buildconfigs_names_list(nodes_names_list)
     imagestreams_names_list = get_imagestreams_names_list(nodes_names_list)
     imagestreams_creation_res = await create_imagestreams(openshift_api, namespace, imagestreams_names_list)
-    # buildconfigs_creation_res = await create_buildconfigs(openshift_api, namespace, buildconfigs_names_list, nodes_names_list,
-    #                                                 imagestreams_names_list)
+    buildconfigs_creation_res = await create_buildconfigs(openshift_api, namespace, buildconfigs_names_list, nodes_names_list,
+                                                    imagestreams_names_list)
 
 
 async def delete_resources(openshift_api: Api, namespace: str, nodes_names_list: list, results_json_list: list) -> None:
@@ -21,4 +21,4 @@ async def delete_resources(openshift_api: Api, namespace: str, nodes_names_list:
     completed_builds_list = get_completed_builds_from_results_json_list(results_json_list)
     images_deletion_res = await delete_images(openshift_api, namespace, completed_builds_list)
     imagestreams_deletion_res = await delete_imagestreams(openshift_api, namespace, imagestreams_names_list)
-    # buildconfig_deletion_res = await delete_buildconfigs(openshift_api, namespace, buildconfigs_names_list)
+    buildconfig_deletion_res = await delete_buildconfigs(openshift_api, namespace, buildconfigs_names_list)
